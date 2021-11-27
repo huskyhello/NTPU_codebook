e1da6539d5679b35040108fa3afc79dbf06e5501
Blue&White <terry25031069@gmail.com>
2021-11-27 11:34:49 +0800

    add dijkstra and bellman ford

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,25 +1,43 @@
-typedef struct Edge{
-    int v; long long len;
-    bool operator > (const Edge &b)const { return len>b.len; }
-} State;
+int e[300][300], d[300], p[300];
 
-const long long INF = 1LL<<60;
+struct node {
+	int b, w;
+	bool operator < (const node& bb)const {
+		return w > bb.w;
+	}
+};
 
-void Dijkstra(int n, vector<Edge> G[], long long d[], int s, int t=-1){
-    static priority_queue<State, vector<State>, greater<State> > pq;
-    while ( pq.size() )pq.pop();
-    for (int i=1; i<=n; i++)d[i]=INF;
-    d[s]=0; pq.push( (State){s,d[s]} );
-    while ( pq.size() ){
-        auto x = pq.top(); pq.pop();
-        int u = x.v;
-        if (d[u]<x.len)continue;
-        if (u==t)return;
-        for (auto &e:G[u]){
-            if (d[e.v] > d[u]+e.len){
-                d[e.v] = d[u]+e.len;
-                pq.push( (State) {e.v,d[e.v]} );
-            }
-        }
-    }
+void dijkstra(int root, int n) {
+	for (int i = 0; i <= n; i++)d[i] = (INT_MAX >> 1);
+	memset(p, 0, sizeof(p));
+	priority_queue<node>yee;
+	d[root] = p[root] = 0;
+	yee.push({ root, d[root] });
+
+	while (!yee.empty()) {
+		node tmp = yee.top(); yee.pop();
+		for (int i = 1; i <= n; i++) {
+			if (d[i]>d[tmp.b] + e[tmp.b][i]) {
+				d[i] = d[tmp.b] + e[tmp.b][i];
+				p[i] = tmp.b;
+				yee.push( { i, d[tmp.b] });
+			}
+		}
+	}
+}
+
+int main() {
+	int n, m, aa, bb, root, cc;
+	while (cin >> n >> m) {
+		memset(e, 0, sizeof(e));
+		for (int i = 0; i <= n; i++)for (int j = 0; j <= n; j++)e[i][j] = (INT_MAX >> 1);
+		for (int i = 0; i < m; i++) {
+			cin >> aa >> bb >> cc;
+			e[aa][bb] = cc;
+		}
+		cin >> root;
+		dijkstra(root, n);
+		for (int i = 1; i <= n; i++)cout << d[i] << " \n"[i==n];
+		for (int i = 1; i <= n; i++)cout << p[i] << " \n"[i==n];
+	}
 }
